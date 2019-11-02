00ac9b4cc1488d8467e6ad188f102785293b378e
nswdbbnswda <nswdbbnswda@163.com>
2019-11-02 11:22:30 +0800

    dir

--- test.c
+++ test.c
@@ -1,3 +1,18 @@
-kill函数就将信号发送给进程或进程组。raise函数择允许进程向自身发送信号。
-int kill(pid_t pid, int signo);
-int raise(int signo) 
+#include"apue.h"
+#include<dirent.h>
+int main(int argc, char *argv[]){
+	DIR *dp;//目录结构指针
+	struct dirent *dirp;
+	printf("%d\n", argc);
+	if(argc != 2)
+		err_quit("usage: ls directory_name");
+		
+	if((dp = opendir(argv[1])) == NULL)
+		err_sys("can't open %s", argv[1]);
+	while((dirp = readdir(dp)) != NULL)
+		printf("%s\n", dirp->d_name);
+	closedir(dp);
+	exit(0);
+}
+
+
